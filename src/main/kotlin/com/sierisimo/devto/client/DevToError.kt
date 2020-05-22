fa5d46bd153392bf8bc8e70a81020c39fe41c0f3
Sinuhe Jaime <sierisimo@gmail.com>
2020-05-22 02:54:01 -0500

    A litle more dynamic error manage

--- src/main/kotlin/com/sierisimo/devto/client/DevToError.kt
+++ src/main/kotlin/com/sierisimo/devto/client/DevToError.kt
@@ -2,7 +2,8 @@ package com.sierisimo.devto.client
 
 data class DevToError(
     val error: String,
-    val status: Int
+    val status: Int,
+    val source: String = "Unknown source"
 ) {
     companion object {
         val unknownError = DevToError("Unknown error happened", 400)
--- src/main/kotlin/com/sierisimo/devto/client/DevToErrorDecoder.kt
+++ src/main/kotlin/com/sierisimo/devto/client/DevToErrorDecoder.kt
@@ -8,13 +8,25 @@ internal class DevToErrorDecoder : ErrorDecoder {
     private val gson: Gson by lazy { Gson() }
 
     override fun decode(methodKey: String?, response: Response?): Exception {
-        //TODO: Specific errors for specif method calls based on methodKey
+        //TODO: Specific errors for specif method calls based on methodKey and response.request
+/*
+        response?.let {
+            println("--> ${it.reason()}")
+            println("--> ${it.request()}")
+        }
+*/
 
-        val error = response?.body()?.asInputStream()?.bufferedReader()?.readText()?.let {
+        var error = response?.readBodyText()?.let {
             gson.fromJson(it, DevToError::class.java)
         } ?: DevToError.unknownError
 
+        if (methodKey != null) {
+            error = error.copy(source = methodKey)
+        }
+
         throw DevToCallException(error)
     }
+
+    private fun Response.readBodyText(): String? = body()?.asInputStream()?.bufferedReader()?.readText()
 }
 
